2d8cd9192d3164359b15d9877f6b2bad003ed41f
OstinT <145394220+OstinT66@users.noreply.github.com>
2025-07-31 12:26:33 -0500

    repositorio js versión 1

---
+++ 1_sintaxis_variable/app.js
@@ -0,0 +1,27 @@
+//alert("hola mundo"); //Crea una ventana emergente//
+//prompt("Dime tu nombre")//Crea una ventana para poner información//
+
+//Variable declarada con let
+
+let personaje = "Asmodeo"
+let puntaje = 0
+let shots = 3                //Los valores númericos van sin comillas ""
+     recarga = 2
+
+console.log("Total balas:", shots + recarga)
+
+//El usuario capturó 4 patos
+puntaje += 4
+//El usuario gastó 2 balas
+shots -= 2
+//El usuario recargó su arma
+recarga += 1 
+recarga ++ //++ Permmite sumar de a uno
+recarga ++ //++ Permmite sumar de a uno
+
+console.log("Puntaje", puntaje)
+console.log("shots", shots)
+console.log("Recarga", recarga)
+
+
+
---
+++ 2_operadores_comparacion_logico/app.js
@@ -0,0 +1,29 @@
+//Boolean 
+
+console.log( ' "5 == 5" ', "5" == "5" ) //true
+console.log( ' "5 === 5" ', "5" === "5" ) //true
+console.log( ' 5 === 5" ', 5 === "5" ) //false
+console.log( ' 5 == 5" ', 5 == "5" ) //true
+console.log( ' 5 === (3+2) ', "5" === (3+2) ) //false
+console.log( ' 5 === 5 ', 5 === 5 ) //true
+
+//concatenar ( + )
+
+console.log( 5 + "3")
+console.log( 5 + 3)
+console.log( 5 * "3")
+
+
+//*Pedir dos datos al usuario y sumarlo
+//let num1 = Number ( prompt( 'dame num1') )
+//let num2 = Number ( prompt( 'dame num2') )
+//let resultado = num1 + num2
+//alert("El resultado de tu suma es " + resultado)
+
+
+alert ( " Dame 2 números ")
+
+let num1 = prompt( 'Valor número 1')
+let num2 = prompt( 'Valor número 2')
+
+let resultado
