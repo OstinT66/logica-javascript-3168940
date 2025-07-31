8459f87720e67ea4ff6ee707cec2aae094851047
OstinT <145394220+OstinT66@users.noreply.github.com>
2025-07-31 13:30:12 -0500

    Ejercicio terminado

---
+++ 2_operadores_comparacion_logico/2_operadores_comparacion_logico/app.js
@@ -0,0 +1,28 @@
+//Boolean 
+
+/*console.log( ' "5 == 5" ', "5" == "5" ) //true
+console.log( ' "5 === 5" ', "5" === "5" ) //true
+console.log( ' 5 === 5" ', 5 === "5" ) //false
+console.log( ' 5 == 5" ', 5 == "5" ) //true
+console.log( ' 5 === (3+2) ', "5" === (3+2) ) //false
+console.log( ' 5 === 5 ', 5 === 5 ) //true
+
+//concatenar ( + )
+
+console.log( 5 + "3")
+console.log( 5 + 3)
+console.log( 5 * "3")*/
+
+
+
+/* 
+alert ( " Escriba 2 números ")
+
+let x = Number( prompt ('Escriba un número'))
+let b = Number( prompt(' Escriba un número'))
+let z = x + b
+
+alert( "La suma de los números es " + z ) */
+
+
+console.log(`Tú número es ${x}`)
