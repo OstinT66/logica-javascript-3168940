f06c7fc68383d6bcc9c7eae1bc0bb135e75c2b17
OstinT <145394220+OstinT66@users.noreply.github.com>
2025-08-04 13:32:56 -0500

    ejercicio js logica

---
+++ 3_condicionales/app.js
@@ -0,0 +1,29 @@
+/* let nota = 5;
+console.log( " He realizado mi examen." );
+
+if (nota < 5) {
+    calificacion = "suspendido";
+}
+if (nota >= 5) {
+    calificacion = "aprobado";
+}
+
+console.log(" estoy ", calificacion); */
+
+alert ( " control ");
+
+let nombre = (prompt ("Escriba su nombre"))
+
+alert (" estamos verificando")
+
+alert (" escriba su edad")
+
+let edad = (prompt ("escriba su edad"))
+
+if (edad < 18){
+    alert (" menores de edad, no pasan")
+} else {
+    edad >= 18;
+    alert ("pase sin problema")
+}
+
