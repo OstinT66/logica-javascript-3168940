30129c50f61d1125ee50f9719c97e1c202fc5eb2
OstinT <145394220+OstinT66@users.noreply.github.com>
2025-08-05 13:27:00 -0500

    Ejercicios terminados

--- 3_condicionales/app.js
+++ 3_condicionales/app.js
@@ -11,11 +11,11 @@ if (nota >= 5) {
 console.log(" estoy ", calificacion); */
 
 
-// Condicionales en cadena
+// Ejercicio 1: 
 
-let nombre = (prompt ("Escriba su nombre"));
+l/* et nombre = (prompt ("Escriba su nombre"));
 
-nombre = nombre.toLocaleLowerCase();
+nombre = nombre.toLocaleLowerCase().trim();
 
 if (nombre == "mario" || nombre == "carlos") {
     alert(" Usted es VIP")
@@ -29,4 +29,26 @@ if (edad < 18){
     alert (" menores de edad, no pasan")
 } else {
     alert ("pase sin problema")
+} */
+
+//EJERCIO 2: 
+
+// Pedir datos al usuario
+let nombre = prompt("¿Cuál es tu nombre?").toLowerCase().trim();
+let estatura = parseInt(prompt("¿Cuál es tu estatura en cm?"));
+let velocidad = parseInt(prompt("¿Cuál es tu velocidad en km/h?"));
+let edad = parseInt(prompt("¿Cuál es tu edad en años?"));
+
+// Validar si el usuario cumple con la estatura y velocidad
+if (estatura >= 170 && velocidad >= 27) {
+  alert("Felicidades, cumples con los requisitos para ingresar al equipo.");
+
+  // Verificar edad para determinar división
+  if (edad < 18) {
+    alert("Eres menor de edad, puedes ingresar a la división juvenil.");
+  } else {
+    alert("Eres mayor de edad, puedes ingresar a la división adulta.");
+  }
+} else {
+  alert("Lo siento, no cumples con los requisitos para ingresar al equipo.");
 }
